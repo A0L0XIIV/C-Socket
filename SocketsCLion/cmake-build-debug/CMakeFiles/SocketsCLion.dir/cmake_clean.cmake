4c8c3d451d7aba6b8c056a0795d49cb0cb99b43f
A0L0XIIV <brnky_95@hotmail.com>
2018-11-24 19:42:08 +0300

    Inital push to the GitHub.
    Signed-off-by: A0L0XIIV <brnky_95@hotmail.com>

---
+++ SocketsCLion/cmake-build-debug/CMakeFiles/SocketsCLion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SocketsCLion.dir/main.cpp.obj"
+  "SocketsCLion.pdb"
+  "SocketsCLion.exe"
+  "SocketsCLion.exe.manifest"
+  "SocketsCLion.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SocketsCLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
